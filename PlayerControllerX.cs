e2b79b628b44e65c03f6f009b0f3abd538b5cba4
Jasper <44803626+jaspervanheusden@users.noreply.github.com>
2022-06-15 15:07:19 +0200

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    private float verticalInput;
+   
+
+    void FixedUpdate()
+    {
+
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * 2);
+
+    }
+}
